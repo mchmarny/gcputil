2e7a904c28f4a08dba32ea902b28b6e77b06a72a
Mark Chmarny <mark@chmarny.com>
2019-06-26 19:32:58 -0700

    stackdriver metrics

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.12
 require (
 	cloud.google.com/go v0.40.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.3.1
 	github.com/stretchr/testify v1.3.0
+	google.golang.org/genproto v0.0.0-20190530194941-fb225487d101
 )
---
+++ pkg/metric/sd_test.go
@@ -0,0 +1,20 @@
+package metric
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetric(t *testing.T) {
+
+	ctx := context.Background()
+	c, err := NewClient(ctx, "test")
+	assert.Nil(t, err)
+	assert.NotNil(t, c)
+
+	err = c.Publish(ctx, "test1", "test-metric", float64(1.23))
+	assert.Nil(t, err)
+
+}
